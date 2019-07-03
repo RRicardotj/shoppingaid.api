c57f6ed6102dce4b80991d8da838b360a0fb2b78
Ricardo Rodriguez <tjrprecursor@gmail.com>
2019-07-03 13:39:33 -0400

    update article

--- src/articles/handlers.js
+++ src/articles/handlers.js
@@ -40,6 +40,48 @@ module.exports = {
     
     return res.json({ articles });
   },
-  updateHandler: async (req, res) => {},
+  updateHandler: async (req, res) => {
+    const { shopId, name, price, quantity, assessment } = req.body;
+    const { id } = req.params;
+
+    if (!shopId || !name || !price || !quantity || !assessment) {
+      let message = `${!shopId ? 'shopId es requerido. ' : ''}`;
+      message += `${!name ? 'El nombre es requerido. ' : ''}`;
+      message += `${!price ? 'El precio es requerido. ' : ''}`;
+      message += `${!quantity ? 'La cantidad es requerida. ' : ''}`;
+      message += `${!assessment ? 'El gravamen es requerido. ' : ''}`;
+
+      throw new ShopError(message);
+    }
+
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+
+    await client.connect();
+
+    const collection = await client.db('shoppingaid').collection('shops');
+    console.log(shopId);
+
+    const shop = await collection.findOne({ _id: new ObjectID(shopId) });
+    
+    if (!shop) {
+      throw new ShopError('La compra no fué encontrada');
+    }
+    let { articles } = shop;
+    if (!articles || articles.length === 0) { throw new ShopError('La compra no tiene artículos'); }
+
+    articles = articles.map((item) => {
+      if (item.idItem === id) {
+        const itemUpdated = {...item, name, price, quantity, assessment};
+        return itemUpdated;
+      }
+      return item;
+    });
+
+    console.log(articles);
+
+    await collection.updateOne({ _id: new ObjectID(shopId) }, { $set: { articles } });
+    
+    return res.json({ articles });
+  },
   removeHandler: async (req, res) => {},
 };
